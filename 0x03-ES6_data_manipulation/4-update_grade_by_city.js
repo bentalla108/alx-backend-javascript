c73d979851de06340690ea1117db8b5d95f8d8e9
bentalla108 <tallambaye108@gmail.com>
2023-07-04 20:19:05 +0000

    Filtering , Mapping and finding

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,13 @@
-
-export default function updateStudentGradeByCity(students, city, newgrades){
-
-    return students.
-    filter(student => student.location=== city)
-    .map(student => {
-      const gradeObject = newgrades.
-      find(grade => grade.studentId === student.id);
+export default function updateStudentGradeByCity(students, city, newgrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newgrades
+        .find((grade) => grade.studentId === student.id);
       const grade = gradeObject ? gradeObject.grade : 'N/A';
       return {
         ...student,
-        grade: grade
+        grade,
       };
-    });;
+    });
 }
