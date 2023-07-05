8ceaff60feebc0e3c09b689ceee1e83d0f2c4a72
bentalla108 <tallambaye108@gmail.com>
2023-07-05 00:03:18 +0000

    Somme feature dont helps ....

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-  const product = new Map();
-  product.set('Apples', 10);
-  product.set('Tomatoes', 10);
-  product.set('Pasta', 1);
-  product.set('Rice', 1);
-  product.set('Banana', 5);
-
-  return product;
-}
+    /* eslint-disable */
+    const map = new Map();
+    map.set('Apples', 10);
+    map.set('Tomatoes', 10);
+    map.set('Pasta', 1);
+    map.set('Rice', 1);
+    map.set('Banana', 5);
+    return map;
+  };
